3138d965aa1c1c3c8695aa67a77d523a0ad99c91
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-12 13:04:45 -0500

    inicio del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent}, //Redirigir al Home
+  {path:'detalle/:id', component:DetalleComponent}, //Redirigir al Detalle
+  // cualquier url redirigir a Home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
